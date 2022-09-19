aa6c5a8a6b6587750699b42a6ae65a3155d35a4f
MiguelSanCar <miguelsanchezcarneiro@gmail.com>
2022-09-19 14:38:32 +0200

    Actualizacion ej4

--- Acceso a Datos/19-09-2022/DirectoriosSinMaven/src/ejercicio4.java
+++ Acceso a Datos/19-09-2022/DirectoriosSinMaven/src/ejercicio4.java
@@ -1,21 +1,23 @@
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class ejercicio4 {
     public static void main(String[] args) throws IOException {
-        FileInputStream entrada = new FileInputStream("C:\\Users\\FP\\Desktop\\dsa\\fichero1.txt");
+        BufferedReader entrada = new BufferedReader(new FileReader("C:\\Users\\FP\\Desktop\\dsa\\fichero1.txt"));
 
 
+        entrada.
         int cosa1 = 0;
         int suma = 0;
         int cont = 0;
-        while (cosa1 != -1) {
+        while (cosa1 != null) {
             System.out.println(cosa1);
             cosa1 = entrada.read();
             suma = suma + cosa1;
             cont++;
             if (cosa1 == |) {
-                
+
             }
 
         }
